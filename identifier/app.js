a6d50881a8117b96433f712578f2c9cb64b61ac5
kimsangbin <tkdqls612@naver.com>
2022-07-31 21:00:09 +0900

    first commit

---
+++ identifier/app.js
@@ -0,0 +1,16 @@
+let age = 10; // age 식별자
+const AGE = 10; //상수는 대문자로 작성
+
+//함수
+function setAge() {}
+
+// 객체
+const o = {
+    age : 10,
+    ['my Name1'] : 'kim' // [데이터] 
+}
+
+o['my Name1'];
+
+
+
